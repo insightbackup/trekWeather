b7f40d0d86a3a1448eb7a792819dcf987df7b579
skimport <skimport@gmail.com>
2019-09-20 14:27:54 -0700

    Read as dataframe

--- src/main/scala/TrekWeather.scala
+++ src/main/scala/TrekWeather.scala
@@ -8,6 +8,8 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.SparkSession._
 
 object TrekWeather {
 	def main(args: Array[String]) {
@@ -15,21 +17,23 @@ object TrekWeather {
 		// Set up spark context
 		val conf = new SparkConf().setAppName("TrekWeather")
 		val sc = new SparkContext(conf)
+		val spark = SparkSession.builder.appName("TrekWeather").
+							config("spark.master", "local").getOrCreate()
+		import spark.implicits._
 
 		// load file from s3
 		val stations = sc.textFile("s3a://kimport-de-data/noaa/ghcnd-stations.txt")
 
 		// split into pieces: ID, lat, lon, state
-		val splitLines = stations.map(line => Array(line.substring(0,11), 
+		val splitLines = stations.map(line => (line.substring(0,11), 
 		      line.substring(12,20), line.substring(21,30), line.substring(38,40)))
 
 		// filter by stations in the US
-		val usStations = splitLines.filter(x => x(0).substring(0,2) == "US")
+		val usStations = splitLines.filter(x => x._1.substring(0,2) == "US")
 
-		// filter further by for only lower 48
-		val lower48 = usStations.filter(x => (x(3) != "AK" && x(3) != "HI"))
+		val stationsDF = usStations.toDF("ID", "Lat", "Lon", "State")
 
-		// confirm by printing first 10
-		lower48.take(10).foreach(x => println(x.deep.mkString(" ")))
+		stationsDF.show()
+
 	}
 }
